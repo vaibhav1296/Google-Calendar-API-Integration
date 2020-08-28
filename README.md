8e0a7fe17beda202f9bf17dd35c167d4a8da2cf0
Vaibhav Sharma <sharmavaibhav5796@gmail.com>
2020-08-28 14:58:41 +0530

    Changes in README and app.js(get-my-events API)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+If you get FetchError and reason: connect ETIMEDOUT 172.217.31.10:443 then it is a known issue, you can check it here
+
+[GitHub](https://github.com/googleapis/google-auth-library-nodejs/issues/283)
+
 Some images of the Calendar API Integration
 
 ![ScreenShot](https://i.imgur.com/WKF8tqd.png)
@@ -9,3 +13,7 @@ Some images of the Calendar API Integration
 ![ScreenShot](https://i.imgur.com/D8ajzkp.png)
 
 ![ScreenShot](https://i.imgur.com/UZHHrZs.png)
+
+After successful event creation and a successful response to attendee
+
+![Screenshot](https://i.imgur.com/gIqmf5S.png)
--- app.js
+++ app.js
@@ -29,12 +29,90 @@ app.post("/events", (req, res) => {
   const eventStartTime = new Date(year, month, date, startHour, startMin);
 
   const eventEndTime = new Date(year, month, date, endHour, endMin);
-  if (startHour <= endHour && startMin <= endMin) {
+
+  console.log("calendar created");
+  if (startHour == endHour) {
+    if (startMin < endMin) {
+      const calender = google.calendar({
+        version: "v3",
+        auth: oAuthClient,
+      });
+      const event = {
+        summary: req.body.summary,
+        description: req.body.description,
+        start: {
+          dateTime: eventStartTime,
+          timeZone: "Asia/Kolkata",
+        },
+        end: {
+          dateTime: eventEndTime,
+          timeZone: "Asia/Kolkata",
+        },
+        attendees: [{ email: req.body.email }],
+        reminders: {
+          useDefault: true,
+        },
+        colorId: 1,
+      };
+      console.log("Event Created");
+      calender.freebusy.query(
+        {
+          resource: {
+            timeMin: eventStartTime,
+            timeMax: eventEndTime,
+            timeZone: "Asia/Kolkata",
+            items: [{ id: "primary" }],
+          },
+        },
+        (err, response) => {
+          if (err) {
+            console.log("free busy error");
+            console.error("FREE BUSY ERROR: ", err);
+            const message = {
+              title: "something wrong with freeBusy method",
+            };
+            res.render("error", { message: message });
+          }
+
+          const eventsArr = response.data.calendars.primary.busy;
+          if (eventsArr.length === 0) {
+            console.log("Event arr length is not 0");
+            calender.events.insert(
+              {
+                calendarId: "primary",
+                resource: event,
+                auth: oAuthClient,
+                sendNotifications: true,
+              },
+              (err) => {
+                if (err) {
+                  console.log("calendar event error");
+                  console.error("CALENDER EVENT ERROR: ", err);
+                  const message = { title: "Calendar Event Error" };
+                  res.render("error", { message: message });
+                } else {
+                  console.log("everything went right");
+                  res.render("success");
+                }
+              }
+            );
+          } else {
+            console.log("event array is busy", eventArr);
+            const message = { title: "You are busy on that day" };
+            res.render("error", { message: message });
+          }
+        }
+      );
+    } else {
+      console.log("startmin is not less than endMin");
+      const message = { title: "Start minute is not less than end minute" };
+      res.render("error", { message: message });
+    }
+  } else if (endHour > startHour) {
     const calender = google.calendar({
       version: "v3",
       auth: oAuthClient,
     });
-
     const event = {
       summary: req.body.summary,
       description: req.body.description,
@@ -52,6 +130,7 @@ app.post("/events", (req, res) => {
       },
       colorId: 1,
     };
+    console.log("event created");
     calender.freebusy.query(
       {
         resource: {
@@ -65,9 +144,12 @@ app.post("/events", (req, res) => {
         if (err) {
           console.log("free busy error");
           console.error("FREE BUSY ERROR: ", err);
-          res.render("error");
+          const message = {
+            title: "Something wrong with freeBusy method",
+          };
+          res.render("error", { message: message });
         }
-
+        console.log(response);
         const eventsArr = response.data.calendars.primary.busy;
         if (eventsArr.length === 0) {
           console.log("Event arr length is not 0");
@@ -82,23 +164,58 @@ app.post("/events", (req, res) => {
               if (err) {
                 console.log("calendar event error");
                 console.error("CALENDER EVENT ERROR: ", err);
-                res.render("error");
+                const message = { title: "Celendar Event Error" };
+                res.render("error", { message: message });
               } else {
                 console.log("everything went right");
                 res.render("success");
               }
             }
           );
+        } else {
+          console.log("event array is busy", eventArr);
+          const message = { title: "You are busy on that day" };
+          res.render("error", { message: message });
         }
-        console.log("Sorry boss I am busy", eventsArr);
       }
     );
   } else {
-    console.log("katai alag error");
-    res.render("error");
+    const message = { title: "start and end timing is incorrect" };
+    res.render("error", { message: message });
   }
+});
+app.get("/get-my-events", (req, res) => {
+  listEvents(oAuthClient);
 });
 
 app.listen(3000, () => {
   console.log("the calendar app is running");
 });
+function listEvents(auth) {
+  console.log("Function invoked");
+  const calendar = google.calendar({ version: "v3", auth });
+  console.log("calendar created");
+  calendar.events.list(
+    {
+      calendarId: "primary",
+      // timeMin: (new Date()).toISOString(),
+      // maxResults: 10,
+      // singleEvents: true,
+      // orderBy: 'startTime',
+    },
+    (err, response) => {
+      if (err) return console.log("The API returned an error: " + err);
+      console.log("No error till events");
+      const events = response.data.items;
+      if (events.length) {
+        console.log("Upcoming 10 events:");
+        events.map((event, i) => {
+          const start = event.start.dateTime || event.start.date;
+          console.log(`${start} - ${event.summary}`);
+        });
+      } else {
+        console.log("No upcoming events found.");
+      }
+    }
+  );
+}
